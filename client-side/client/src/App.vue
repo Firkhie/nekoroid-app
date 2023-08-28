f23a1c745a6083dfbef6c11684a80aac83e483c2
Firdig Alfalakhi <Alfalakhie@gmail.com>
2023-08-28 19:02:57 +0700

    init project vue with tailwind

---
+++ client-side/client/src/App.vue
@@ -0,0 +1,12 @@
+<script setup>
+import { RouterView } from 'vue-router'
+</script>
+
+<template>
+  <h1 class="text-3xl font-bold underline text-yellow-500">Hello world!</h1>
+  <p>Test</p>
+
+  <!-- <RouterView /> -->
+</template>
+
+<style scoped></style>
